9e622218db301ce9f1e929c6229c540f66e90de8
A.P Sim <apsim@ucdavis.edu>
2020-01-28 15:33:59 -0800

    Add git cheatsheet

---
+++ git-commands-cheatsheet.R
@@ -0,0 +1,17 @@
+# Shows the repository your local is pointing to
+##  in terminal: git removes -v
+##  in R, usethis::git_remotes()
+
+# To go back in time:
+##  git checkout -b time-travel [SHA]
+##  git log --pretty=oneline
+##  the "time-travel" is now the human name for 9b1a46e0
+
+# To go back to the present:
+## git checkout master
+
+# To revert
+##  git revert --no-edit 09d03eb
+
+# To go back in time and get only one file
+##  git checkout 27a81f336 --filename.ext
